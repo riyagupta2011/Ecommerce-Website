21054a00eaa91c103a0ea7da70244f5613978630
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-16 17:04:15 +0530

    slider completed

---
+++ ecommerce/src/data.js
@@ -0,0 +1,24 @@
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://fashinza.com/textile/wp-content/uploads/2022/12/safeimagekit-shutterstock_591829421.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "#f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://im.rediff.com/movies/2019/jul/05movie5.jpg?w=670&h=900",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "#fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://media.theeverygirl.com/wp-content/uploads/2021/07/the-everygirl-summer-style-tips-feature.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "#fbf0f4",
+    },
+  ];
