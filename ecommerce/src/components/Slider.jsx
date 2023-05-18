495d117ad946ae0d0b5e85e9b08fc0f1bc619016
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-18 23:20:33 +0530

    product component completed

--- ecommerce/src/components/Slider.jsx
+++ ecommerce/src/components/Slider.jsx
@@ -90,7 +90,7 @@ function Slider() {
         </Arrow>
         <Wrapper slideIndex={slideIndex}>
           {sliderItems.map(item=>(
-              <Slide bg={item.bg}>
+              <Slide bg={item.bg} key={item.id}>
               <ImageContainer>
                <Image src={item.img}/>
               </ImageContainer>
--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+import Products from '../components/Products'
 
 
 function Home() {
@@ -10,6 +12,8 @@ function Home() {
       <Announcement/>
        <Navbar/>
        <Slider/>
+       <Categories/>
+       <Products/>
     </div>
   )
 }
