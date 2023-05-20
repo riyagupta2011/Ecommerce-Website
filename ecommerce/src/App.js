b2e437f65fc94c2e22f37ba77a5d747e36784803
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-20 23:45:20 +0530

    individual product page completed

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,13 +1,14 @@
 
 
 import Home from './pages/Home';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 
 function App() {
   return (
     <div className="App">
-     <ProductList/>
+   <Product/>
     </div>
   );
 }
