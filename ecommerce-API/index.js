2e882d4f3a884a0f8c4948ff1cae64d1418ec34f
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-27 14:54:07 +0530

    NodeJs MongoDb authentication

--- ecommerce-API/index.js
+++ ecommerce-API/index.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 const userRoute=require("./routes/user")
+const authRoute=require("./routes/auth")
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL )
     .then(()=>console.log("DB Connection Successfull!"))
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL )
     });
     app.use(express.json());
 app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 app.listen(process.env.PORT||5000,()=>{
     console.log("Backend server is running")
 })
--- ecommerce-API/models/User.js
+++ ecommerce-API/models/User.js
@@ -5,7 +5,7 @@ const UserSchema=new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     isAdmin:{
-        type:bBoolean,
+        type:Boolean,
         default:false,
     },
     
