9b3cb3526b20e636ecb9d94ef2caccf62afbe101
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-28 16:01:54 +0530

    Add user

--- ecommerce-API/routes/user.js
+++ ecommerce-API/routes/user.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const {
   verifyToken,
   verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
   
 } = require("./verifyToken");
 
@@ -29,4 +30,30 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//DELETE
+router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
+   try{
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted...")
+
+   }catch(err)
+   {
+    res.status(500).json(err)
+   }
+})
+
+//GET USER
+router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
+    try{
+     const user=await User.findById(req.params.id)
+     const { password, ...others } = user._doc;
+     res.status(200).json(others);
+ 
+    }catch(err)
+    {
+     res.status(500).json(err)
+    }
+ })
+ 
 module.exports=router
