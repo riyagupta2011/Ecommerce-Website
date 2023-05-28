b2bf75daf9bf124e20a5b8d851e0bc157baede16
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-28 15:51:32 +0530

    updation of user done

--- ecommerce-API/routes/user.js
+++ ecommerce-API/routes/user.js
@@ -1,10 +1,32 @@
-const router=require("express").Router()
-router.get("/usertest",(req,res)=>{
-    res.send("user test is successfull");
-});
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  
+} = require("./verifyToken");
 
-router.post("/userposttest",(req,res)=>{
-    const username=req.body.username
-    res.send("your username is :"+username)
-})
+const router = require("express").Router();
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports=router
