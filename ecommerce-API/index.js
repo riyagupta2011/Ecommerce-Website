0f696c6259dd0335672581473d93a605c9e1f9ad
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-28 22:17:36 +0530

    completed rest API

--- ecommerce-API/index.js
+++ ecommerce-API/index.js
@@ -5,17 +5,24 @@ const dotenv=require("dotenv")
 const userRoute=require("./routes/user")
 const authRoute=require("./routes/auth")
 const productRoute=require("./routes/product")
+const orderRoute=require("./routes/order")
+const cartRoute=require("./routes/cart")
+
 dotenv.config();
+
+
 mongoose.connect(process.env.MONGO_URL )
     .then(()=>console.log("DB Connection Successfull!"))
     .catch((err)=>{
         console.log(err);
     });
-    app.use(express.json());
+
+app.use(express.json());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/products",productRoute);
-
+app.use("/api/orders",orderRoute);
+app.use("/api/carts",cartRoute);
 
 app.listen(process.env.PORT||5000,()=>{
     console.log("Backend server is running")
