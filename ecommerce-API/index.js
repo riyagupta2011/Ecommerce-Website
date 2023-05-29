f0654fdafac717a340a1365f8bcc978719445bdc
riyagupta2011 <riyagupta75751@gmail.com>
2023-05-29 11:19:34 +0530

    Stripe backend done

--- ecommerce-API/index.js
+++ ecommerce-API/index.js
@@ -7,6 +7,8 @@ const authRoute=require("./routes/auth")
 const productRoute=require("./routes/product")
 const orderRoute=require("./routes/order")
 const cartRoute=require("./routes/cart")
+const stripeRoute=require("./routes/stripe")
+const cors=require("cors")
 
 dotenv.config();
 
@@ -17,12 +19,14 @@ mongoose.connect(process.env.MONGO_URL )
         console.log(err);
     });
 
+app.use(cors());    
 app.use(express.json());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/products",productRoute);
 app.use("/api/orders",orderRoute);
 app.use("/api/carts",cartRoute);
+app.use("/api/checkout",stripeRoute);
 
 app.listen(process.env.PORT||5000,()=>{
     console.log("Backend server is running")
