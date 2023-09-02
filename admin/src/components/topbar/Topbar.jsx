aece8627ed81d381a6f036d20eb7bfc858fa987c
riyagupta2011 <riyagupta75751@gmail.com>
2023-09-02 11:22:31 +0530

    started admin portal

---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./topbar.css"
+const Topbar = () => {
+  return (
+    <div className="topbar">
+        <div className="topbarWrapper">
+            <div className="topLeft">left</div>
+            <div className="topRight">right</div>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
